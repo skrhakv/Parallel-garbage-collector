e1f31f623e244cde2ca58d06649ea22148d3c7f6
skrhakv <vitek.skrhak@seznam.cz>
2021-05-17 11:26:49 +0200

    hopefully final version

--- gc.cpp
+++ gc.cpp
@@ -2,81 +2,214 @@
 #include <list>
 #include "gc.h"
 
-gc_object head_gc_obj;
-gc_object * actual_gc_obj = &head_gc_obj;
-
 gc_object::gc_object()
 {
-
+    if (DEBUG)
+        std::cout << "normal constructor" << std::endl;
+    prev = actual_obj;
+    actual_obj->next = this;
+    actual_obj = this;
 }
 
-void gc_object::get_ptrs(std::function<void(gc_object *)> callback)
+gc_object::gc_object(const gc_object &)
 {
-    std::cout << "get pointers in base class" << std::endl;
+    if (DEBUG)
+        std::cout << "copy constructor" << std::endl;
+    prev = actual_obj;
+    actual_obj->next = this;
+    actual_obj = this;
 }
 
-gc_root_ptr_base head_root;
-gc_root_ptr_base *actual_root = &head_root;
-
-template <typename T>
-gc_root_ptr<T>::gc_root_ptr(T *p)
+gc_object &gc_object::operator=(const gc_object &)
 {
-    //gc_root_ptr_base* moi = this;
-    //root_objs.push_back((gc_root_ptr_base*)this);
-    pt = p;
-    prev = actual_root;
-    actual_root->next = this;
-    actual_root = this;
-    next = nullptr;
+    if (DEBUG)
+        std::cout << "copy assignment" << std::endl;
+
+    return *this;
 }
 
-template <typename T>
-gc_root_ptr<T>::~gc_root_ptr()
+gc_object::~gc_object()
 {
-    if (actual_root == this)
-    {
-        actual_root = prev;
-    }
+    if (DEBUG)
+        std::cout << "let's call it done chaps! (~gc_object)" << std::endl;
+    if (actual_obj == this)
+        actual_obj = prev;
+
+    // if prev/next are not null then keep the list valid
     if (prev)
-    {
         prev->next = next;
-    }
+
     if (next)
-    {
         next->prev = prev;
+}
+
+gc_object_base gc_object_base::head_obj;
+gc_object_base *gc_object_base::actual_obj = &head_obj;
+
+gc_root_ptr_base gc_root_ptr_base::head_root;
+gc_root_ptr_base *gc_root_ptr_base::actual_root = &head_root;
+
+void gc::callback(gc_object *object)
+{
+    if (!object)
+        return;
+    
+    object->reachability_flag = true;
+
+    // we don't need any complicated synchonization here (it doesn't really matter if we spawn another job even if race condition occures)
+    if (fork_counter > 0)
+    {
+        fork_counter -= 1;
+        job_counter += 1;
+        add_job(object);
+        return;
+    }
+    object->get_ptrs(callback);
+}
+void gc::threadpool_loop()
+{
+    while (true)
+    {
+        gc_object *job = nullptr;
+
+        {
+            std::unique_lock<std::mutex> lock(threadpool_mutex);
+
+            threadpool_condition.wait(lock, [&]()
+                                      { return !queue.empty() || terminate_pool; });
+            if (!queue.empty())
+            {
+                job = queue.front();
+                queue.pop();
+            }
+        }
+        if (job != nullptr)
+        {
+            job->get_ptrs(callback);
+
+            // notify other threads that one thread has just finished (to spawn new job)
+            fork_counter += 1;
+
+            thread_finish_counter += 1;
+
+            // if number of finished jobs equals job counter, then wake up the main thread
+            if (thread_finish_counter == job_counter)
+                end_of_marking_condition.notify_one();
+        }
+        else
+            break;
     }
 }
-template <typename T>
-T *gc_root_ptr<T>::operator->() const { return pt; }
 
-template <typename T>
-T &gc_root_ptr<T>::operator*() const { return *pt; }
-
-template <typename T>
-T *gc_root_ptr<T>::get() const
+void gc::add_job(gc_object *New_Job)
 {
-    return this->pt;
+    {
+        std::unique_lock<std::mutex> lock(add_job_mutex);
+        queue.push(New_Job);
+    }
+    threadpool_condition.notify_one();
 }
 
-template <typename T>
-void gc_root_ptr<T>::reset(T *ptr)
+void gc::terminate_threads()
 {
-    this->pt = ptr;
-}
+    {
+        std::unique_lock<std::mutex> lock(shutdown_mutex);
+        terminate_pool = true;
+    }
 
-template <typename T>
-gc_root_ptr<T>::operator bool() const
+    threadpool_condition.notify_all();
+    for (std::thread &every_thread : pool)
+    {
+        every_thread.join();
+    }
+
+    pool.clear();
+    stopped = true;
+}
+void gc::start_threadpool()
 {
-    if (pt)
-        return true;
-    return false;
+    hw_threads = std::thread::hardware_concurrency();
+    for (int i = 0; i < hw_threads; i++)
+    {
+        pool.push_back(std::thread(threadpool_loop));
+    }
+    terminate_pool = false;
+    stopped = false;
 }
 
 void gc::collect()
 {
-    auto iterator = &head_root;
-    while (iterator->next)
+    if (stopped)
     {
-        iterator = iterator->next;
+        start_threadpool();
     }
+    auto mark_iterator = gc_root_ptr_base::head_root.next;
+    fork_counter = hw_threads;
+    thread_finish_counter = 0;
+    job_counter = 0;
+    while (mark_iterator)
+    {
+        if (mark_iterator->gc_object_pointer)
+        {
+            mark_iterator->gc_object_pointer->reachability_flag = true;
+
+            // check if we are at the end gc_root_ptr list
+            //if(mark_iterator->next)
+            {
+                add_job(mark_iterator->gc_object_pointer);
+                job_counter++;
+            }
+        }
+        mark_iterator = mark_iterator->next;
+    }
+
+    {
+        std::unique_lock<std::mutex> myLock(wait_mutex);
+
+        end_of_marking_condition.wait(myLock, [&]()
+                                      { return thread_finish_counter == job_counter; });
+    }
+    // sweeping
+    gc_object_base *sweep_iterator = gc_object_base::head_obj.next;
+    while (sweep_iterator)
+    {
+        if (DEBUG)
+            std::cout << "Sweep it boys" << std::endl;
+        if (!(sweep_iterator->reachability_flag))
+        {
+            gc_object_base *old_it = sweep_iterator;
+            sweep_iterator = sweep_iterator->next;
+            if (DEBUG)
+                std::cout << "Delete!" << std::endl;
+            delete old_it;
+
+            if (DEBUG)
+                std::cout << "No segFault?" << std::endl;
+        }
+        else
+        {
+            sweep_iterator->reachability_flag = false;
+            sweep_iterator = sweep_iterator->next;
+        }
+    }
+    if (gc_object_base::actual_obj == &(gc_object_base::head_obj))
+        terminate_threads();
 }
+std::condition_variable gc::threadpool_condition;
+std::condition_variable gc::end_of_marking_condition;
+
+std::mutex gc::shutdown_mutex;
+std::mutex gc::add_job_mutex;
+std::mutex gc::wait_mutex;
+std::mutex gc::threadpool_mutex;
+
+std::queue<gc_object *> gc::queue;
+std::vector<std::thread> gc::pool;
+
+bool gc::terminate_pool = false;
+bool gc::stopped = true;
+int gc::hw_threads;
+
+std::atomic<int> gc::thread_finish_counter = 0;
+std::atomic<int> gc::job_counter = 0;
+std::atomic<int> gc::fork_counter = 0;
--- gc.h
+++ gc.h
@@ -3,7 +3,6 @@
 
 #include <functional>
 #include <iostream>
-#include <list>
 #include <vector>
 #include <thread>
 #include <mutex>
@@ -15,71 +14,41 @@
 
 class gc_object_base
 {
+private:
     friend class gc_object;
     friend class gc;
+
+    // indicates if object should be deleted when sweeping
     bool reachability_flag = false;
+
+    // prev & next for gc_object's list
     gc_object_base *prev = nullptr;
     gc_object_base *next = nullptr;
+
+    // static head & tail for gc_object's list
+    static gc_object_base head_obj;
+    static gc_object_base *actual_obj;
 
 public:
     virtual ~gc_object_base() {}
 };
 
-// head & tail for gc_object
-gc_object_base head_obj;
-gc_object_base *actual_obj = &head_obj;
 
 class gc_object : gc_object_base
 {
     friend class gc;
 
 public:
-    gc_object()
-    {
-        if (DEBUG)
-            std::cout << "normal constructor" << std::endl;
-        prev = actual_obj;
-        actual_obj->next = this;
-        actual_obj = this;
-    }
-    gc_object(const gc_object &)
-    {
-        if (DEBUG)
-            std::cout << "copy constructor" << std::endl;
-        prev = actual_obj;
-        actual_obj->next = this;
-        actual_obj = this;
-    }
+    gc_object();
+    gc_object(const gc_object &);
     gc_object(gc_object &&) = delete; // move constructor is actually never called (copy constructor is called instead)
-    gc_object &operator=(const gc_object &)
-    {
-        if (DEBUG)
-            std::cout << "copy assignment" << std::endl;
-
-        return *this;
-    }
+    gc_object &operator=(const gc_object &);
     gc_object &operator=(const gc_object &&) = delete; // move assignment is actually never called (copy constructor is called instead)
-    ~gc_object()
-    {
-        if (DEBUG)
-            std::cout << "let's call it done chaps! (~gc_object)" << std::endl;
-        if (actual_obj == this)
-            actual_obj = prev;
-
-        // if prev/next are not null then keep the list valid
-        if (prev)
-            prev->next = next;
-
-        if (next)
-            next->prev = prev;
-    }
+    ~gc_object();
 
 protected:
     virtual void get_ptrs(std::function<void(gc_object *)>) {}
 };
-
-template <typename T>
-class gc_root_ptr;
 
 class gc_root_ptr_base
 {
@@ -88,14 +57,16 @@ class gc_root_ptr_base
     friend class gc;
 
     gc_object *gc_object_pointer = nullptr;
+
+    // prev & next for gc_root_ptr's list
     gc_root_ptr_base *prev = nullptr;
     gc_root_ptr_base *next = nullptr;
+
+    // static head & tail for gc_root_ptr's list
+    static gc_root_ptr_base head_root;
+    static gc_root_ptr_base *actual_root;
 };
 
-
-// head & tail of gc_root_ptr list
-gc_root_ptr_base head_root;
-gc_root_ptr_base *actual_root;
 class gc
 {
 private:
@@ -121,153 +92,15 @@ private:
     static std::atomic<int> job_counter;
     static std::atomic<int> fork_counter;
 
-    static void callback(gc_object *object)
-    {
-        if (!object)
-            return;
-        // we don't need any complicated synchonization here (it doesn't really matter if we spawn another job even if race condition occures)
-        // std::cout << fork_counter << std::endl;
-
-        object->reachability_flag = true;
-        if (fork_counter > 0)
-        {
-            fork_counter -= 1;
-            job_counter += 1;
-            add_job(object);
-            return;
-        }
-        object->get_ptrs(callback);
-    }
-    static void threadpool_loop()
-    {
-        while (true)
-        {
-            gc_object *job = nullptr;
-
-            {
-                std::unique_lock<std::mutex> lock(threadpool_mutex);
-
-                threadpool_condition.wait(lock, [&]()
-                                          { return !queue.empty() || terminate_pool; });
-                if (!queue.empty())
-                {
-                    job = queue.front();
-                    queue.pop();
-                }
-            }
-            if (job != nullptr)
-            {
-                job->get_ptrs(callback); 
-                fork_counter += 1;
-                //std::cout << "increase fork_counter: "<< fork_counter << std::endl;
-                thread_finish_counter += 1;
-                //std:: cout << thread_finish_counter << '\t' << job_counter << std::endl;
-
-                if (thread_finish_counter == job_counter)
-                    end_of_marking_condition.notify_all();
-            }
-            else
-                break;
-        }
-    };
-    static void add_job(gc_object *New_Job)
-    {
-        {
-            std::unique_lock<std::mutex> lock(add_job_mutex);
-            queue.push(New_Job);
-        }
-        threadpool_condition.notify_one();
-    }
-    static void terminate_threads()
-    {
-        {
-            std::unique_lock<std::mutex> lock(shutdown_mutex);
-            terminate_pool = true;
-        }
-
-        threadpool_condition.notify_all();
-        for (std::thread &every_thread : pool)
-        {
-            every_thread.join();
-        }
-
-        pool.clear();
-        stopped = true; 
-    }
+    static void callback(gc_object *object);
+    static void threadpool_loop();
+    static void add_job(gc_object *New_Job);
+    static void terminate_threads();
 
 public:
     gc() {}
-    static void start_threadpool()
-    {
-        hw_threads = std::thread::hardware_concurrency();
-        for (int i = 0; i < hw_threads; i++)
-        {
-            pool.push_back(std::thread(threadpool_loop));
-        }
-        terminate_pool = false;
-        stopped = false;
-    }
-    static void collect()
-    {
-        if (stopped)
-        {
-            start_threadpool();
-        }
-        auto mark_iterator = head_root.next;
-        fork_counter = hw_threads;
-        thread_finish_counter = 0;
-        job_counter = 0;
-        while (mark_iterator)
-        {
-            if (mark_iterator->gc_object_pointer)
-            {
-                mark_iterator->gc_object_pointer->reachability_flag = true;
-
-                // check if we are at the end gc_root_ptr list
-                //if(mark_iterator->next)
-                {
-                    add_job(mark_iterator->gc_object_pointer);
-                    job_counter++;
-                }
-               
-            }
-            mark_iterator = mark_iterator->next;
-        }
-        
-        {
-            std::unique_lock<std::mutex> myLock(wait_mutex);
-
-            end_of_marking_condition.wait(myLock, [&]()
-                                          { return thread_finish_counter == job_counter; });
-        }
-        // sweeping
-        gc_object_base *sweep_iterator = head_obj.next;
-        //std::cout << "here1" << std::endl;
-
-        while (sweep_iterator)
-        {
-            if (DEBUG)
-                std::cout << "Sweep it boys" << std::endl;
-            if (!(sweep_iterator->reachability_flag))
-            {
-                gc_object_base *old_it = sweep_iterator;
-                sweep_iterator = sweep_iterator->next;
-                if (DEBUG)
-                    std::cout << "Delete!" << std::endl;
-                delete old_it;
-
-                if (DEBUG)
-                    std::cout << "No segFault?" << std::endl;
-            }
-            else
-            {
-                sweep_iterator->reachability_flag = false;
-                sweep_iterator = sweep_iterator->next;
-            }
-        }
-        if (actual_obj == &head_obj)
-            terminate_threads();
-    }
+    static void start_threadpool();
+    static void collect();
 };
 template <typename T>
 class gc_root_ptr : gc_root_ptr_base
@@ -278,10 +111,6 @@ private:
 public:
     gc_root_ptr()
     {
-        if (!actual_root)
-        {
-            actual_root = &head_root;
-        }
         prev = actual_root;
         actual_root->next = this;
         actual_root = this;
@@ -394,25 +223,6 @@ public:
     }
 };
 
-std::condition_variable gc::threadpool_condition;
-std::condition_variable gc::end_of_marking_condition;
-
-std::mutex gc::shutdown_mutex;
-std::mutex gc::add_job_mutex;
-std::mutex gc::wait_mutex;
-std::mutex gc::threadpool_mutex;
-
-std::queue<gc_object *> gc::queue;
-std::vector<std::thread> gc::pool;
-
-bool gc::terminate_pool = false;
-bool gc::stopped = true;
-int gc::hw_threads;
-
-std::atomic<int> gc::thread_finish_counter = 0;
-std::atomic<int> gc::job_counter = 0;
-std::atomic<int> gc::fork_counter = 0;
-
 #endif
 
-// g++ -std=c++20 -Wall -Wextra -Wpedantic main.cpp -o main && ./main
+// g++ -o main -std=c++17  -Wall -Wextra -Wpedantic -pthread gc.cpp recodex_main.cpp && ./main 6
